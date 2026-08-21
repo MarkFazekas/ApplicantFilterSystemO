4f72648b2d95401e2a44a376b46941db262afdb3
Márk <10902799+MarkFazekas@users.noreply.github.com>
2026-08-21 16:36:36 +0200

    feat: 3.12 starting position - AFS-000

--- manager_backend/chalicelib/infrastructure/db.py
+++ manager_backend/chalicelib/infrastructure/db.py
@@ -1,12 +1,9 @@
 from functools import cache
-from typing import TYPE_CHECKING
 
 import boto3
+from types_boto3_dynamodb.service_resource import DynamoDBServiceResource, Table
 
 from chalicelib import settings
-
-if TYPE_CHECKING:
-    from types_boto3_dynamodb.service_resource import DynamoDBServiceResource, Table
 
 dynamodb: DynamoDBServiceResource = boto3.resource("dynamodb")
 
